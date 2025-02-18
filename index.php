c7324cad30eb21632aab56bd34b2a9a992074440
waytix-l <lucas.savy7@gmail.com>
2025-02-18 10:13:10 +0100

    accueil

--- index.php
+++ index.php
@@ -4,8 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="src/css/style.css">
 </head>
 <body>
-    INDEX
+    
+    <nav>
+
+        <div class="title">
+            <h1>PHONE</h1>
+        </div>
+
+        <div class="searchBar">
+            <input type="search" name="" id="" placeholder="Search for article">
+        </div>
+
+        <div class="CTANav">
+
+            <ul>
+                <li><a href="">Creer un article</a></li>
+                <li><a href="">Wishlist</a></li>
+                <li><a href="">Panier</a></li>
+            </ul>
+
+            <div class="user">
+                <p>LOGIN</p>
+            </div>
+
+        </div>
+
+    </nav>
+
+    <div class="landing">
+
+    </div>
+
 </body>
 </html>
